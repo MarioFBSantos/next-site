012574bc56807a244ea5fc5ae47e8b3254b17dda
MarioSantos <mariosantos3197@gmail.com>
2021-06-28 21:51:02 -0300

    1.1.0 Navbar js error

--- pages/contato.js
+++ pages/contato.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { Container, Row, Card, Button } from 'react-bootstrap'
-import Navbar from '../components/Navbar'
+import Navbar from '../components/navbar.js'
 import withTransition from "../HOC/withTransition"
 
 function Contato() {
